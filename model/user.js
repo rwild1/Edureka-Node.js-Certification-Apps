00ef7d8ed9a3398d535570bc05c464c246236582
mmabbas <muhammad98abbas@gmail.com>
2021-03-26 15:47:57 -0500

    Login and Register

--- model/user.js
+++ model/user.js
@@ -5,10 +5,9 @@ import mongoose from 'mongoose'
 //define my model
 var userSchema= mongoose.Schema({
     name:{type:String},
-    address:{type:String},
     email:{type:String},
-    //date:{type:Date, default:Date.now},
-    //orders:{type:any}
+    password:{type:String},
+    isAdmin:{type:Boolean}
 },
 {
     collection:"users"
