d0f8f13b3486a2257f4d8ef925f0ffc03b642a39
rwild <robertwildjr@gmail.com>
2021-03-26 15:52:25 -0700

    commit

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -56,12 +56,12 @@ router.get('/newsForm', (req, res) => {
       res.render('newsForm', {
         user,
         msg: req.query.msg ? req.query.msg:''
-      })
-    })
-  })
-})
+      });
+    });
+  });
+});
 
-// post request to add news
+// post request to create/add news
 router.post('/addNews', (req, res) => {
   console.log("/addNews: req.body: ", req.body)
   const token = localStorage.getItem('authtoken')
@@ -97,6 +97,91 @@ router.post('/addNews', (req, res) => {
         res.redirect('/admin/newsForm/?msg=' + msg)
       });
     });
+  });
+});
+
+// get request for news list
+router.get('/newsForm', (req, res) => {
+  const token = localStorage.getItem('authtoken')
+  console.log("token: ", token)
+  if (!token) {
+    res.redirect('/')
+  }
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      res.redirect('/')
+    }
+
+    user.findById(decoded.id, {password: 0}, (err, user) => {
+      if (err) {
+        res.redirect('/')
+      }
+      if (!user) {
+        res.redirect('/')
+      }
+      console.log("/newsForm: user: ", user)
+
+      news.find({}, (err, data) => {
+        if (err) {
+          res.status(500).send(err)
+        } else {
+          res.render('newsList', {
+            user,
+            data
+          })
+        }
+      });
+    });
+  });
+});
+
+// post request to find news by id for modal view
+router.post('/findById', (req, res) => {
+  const id = req.body.id
+  console.log("findById: id: ", id)
+
+  news.find({_id: id}, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      console.log("findById: data: ", data)
+      res.send(data)
+    }
+  });
+});
+
+// put request to edit/update news
+router.put('/updateNews', (req, res) => {
+  const id = req.body.id
+  console.log("updateNews: id: ", id)
+
+  news.findOneAndUpdate({_id: id}, {
+    $set: {
+      title: req.body.title,
+      description: req.body.description,
+      url: req.body.url,
+      urlToImage: req.body.urlToImage,
+      publishedAt: req.body.publishedAt,
+      insertTime: Date.now(0)
+    }
+  }, {
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send("Updated")
+  });
+});
+
+// delete request to delete news
+router.delete('/deleteNews', (req, res) => {
+  const id = req.body.id
+  console.log("deleteNews: id: ", id)
+
+  news.findOneAndDelete({_id: id}, (err, result) => {
+    if (err) return res.status(500).send(err)
+    res.send({message: 'Deleted'})
+    console.log(result)
   });
 });
 
